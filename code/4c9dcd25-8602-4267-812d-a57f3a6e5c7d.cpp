36b55184a2e22ea11659f146625cc6d351cf508e
Vikaass-08 <pathakvikas190@gmail.com>
2021-12-19 15:14:31 +0530

    Run Code

---
+++ code/4c9dcd25-8602-4267-812d-a57f3a6e5c7d.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main(){std::cout<<(5*34)<<endl return 0;}
---
+++ code/d6d2bf84-4a11-4f86-af33-559d5c09a02f.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main(){std::cout<<(3+2)<<endl;return 0;}
